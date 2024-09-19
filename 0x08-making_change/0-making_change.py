af1ce24084413eabcd26d06262f4581c8fc399f3
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-09-19 14:03:15 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-""" Create a DP array to store the fewest coins for each amount up to total"""
+""" Making changes using Dynamic Programming """
 
 
 def makeChange(coins, total):
+    """ Generate the fewest number of coins needed to meet total
+
+    Args:
+        coins (List[int]): List of coins available
+        total (int): total amount needed
+    Returns:
+        int: fewest number of coins needed to meet total, or -1 if not possible
+    """
     if total <= 0:
         return 0
 
-    # Create a DP array to store the fewest coins for each amount up to total
+    # Initialize DP array to store minimum coins required for each amount
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    dp[0] = 0  # 0 coins are needed to make a total of 0
 
-    # Iterate over each coin and calculate the minimum coins required
+    # Iterate over each coin
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
 
+    # If dp[total] is still infinity, return -1 because total can't be reached
     return dp[total] if dp[total] != float('inf') else -1
