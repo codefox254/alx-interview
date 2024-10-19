17279e696fe68f2ff01a070a93c35586ac549d11
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-10-19 08:28:34 -0700

    Update 0-pascal_triangle.py final

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,7 +15,7 @@ def pascal_triangle(n):
         for j in range(len(triangle[i - 1]) - 1):
             row.append(
                 triangle[i - 1][j] + triangle[i - 1][j + 1]
-            )  # Breaking long line
+            )
         row.append(1)
         triangle.append(row)
 
