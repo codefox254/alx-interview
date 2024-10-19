883e29b7cdfa22f08ba8c40b6be5b041b42b83c9
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-10-19 04:13:43 -0700

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" 0-pascal_triangle"""
+"""0-pascal_triangle: Generates Pascal's triangle up to n rows."""
+
 
 def pascal_triangle(n):
-    """ Returns a list of lists of integersrepresenting the Pascal's triangle of n."""
+    """Returns a list of lists of integers representing Pascal's triangle of n."""
     
     triangle = []
     if n <= 0:
         return triangle
+    
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
@@ -14,5 +16,5 @@ def pascal_triangle(n):
             row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
         row.append(1)
         triangle.append(row)
-        
+    
     return triangle
